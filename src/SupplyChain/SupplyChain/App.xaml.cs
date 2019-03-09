61656f25a5622f9906f78638bb0f79b27ab42258
Yuriy Pelekh <ypelekh@softserveinc.com>
2019-03-09 11:32:44 +0200

    Added Database.Versioning package.

--- src/SupplyChain/SupplyChain/App.xaml.cs
+++ src/SupplyChain/SupplyChain/App.xaml.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Windows;
 using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
 
 namespace SupplyChain
 {
@@ -13,5 +8,16 @@ namespace SupplyChain
     /// </summary>
     public partial class App : Application
     {
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["Main"].ConnectionString;
+            var database = new Database.Versioning.Database(connectionString);
+            const string databaseName = "ITS";
+            if (!database.Exists(databaseName))
+            {
+                database.Create(databaseName);
+            }
+            database.Update();
+        }
     }
 }
