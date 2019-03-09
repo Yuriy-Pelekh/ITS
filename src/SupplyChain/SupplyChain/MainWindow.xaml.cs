aefc60a76413ef0d82039d2b906b746651b14b57
Yuriy Pelekh <ypelekh@softserveinc.com>
2019-03-09 20:39:38 +0200

    Added basic functionality.

--- src/SupplyChain/SupplyChain/MainWindow.xaml.cs
+++ src/SupplyChain/SupplyChain/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using SupplyChain.ViewModels;
 
 namespace SupplyChain
 {
@@ -10,6 +11,7 @@ namespace SupplyChain
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
         }
     }
 }
---
+++ src/SupplyChain/SupplyChain/ViewModels/UserViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace SupplyChain.ViewModels
+{
+    public class UserViewModel : ViewModelBase
+    {
+        private int _id;
+        private string _firstName;
+        private string _lastName;
+        private DateTime _updatedDate;
+
+        public int Id
+        {
+            get { return _id; }
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                if (_firstName != value)
+                {
+                    _firstName = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                if (_lastName != value)
+                {
+                    _lastName = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public DateTime UpdatedDate
+        {
+            get { return _updatedDate; }
+            set
+            {
+                if (_updatedDate != value)
+                {
+                    _updatedDate = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public UserViewModel()
+        {
+
+        }
+
+        public UserViewModel(Entities.User user)
+        {
+            _id = user.Id;
+            _firstName = user.FirstName;
+            _lastName = user.LastName;
+            _updatedDate = user.UpdatedDate;
+        }
+    }
+}
