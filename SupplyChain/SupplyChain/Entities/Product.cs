2fa38a5c29f4ce61196a2c414de74e6cbccd6ac3
Rubi <robert.turanicua@gmail.com>
2019-04-23 10:12:11 +0300

    Table
    Have added a table products and connecetd it with DB.

---
+++ SupplyChain/SupplyChain/Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace SupplyChain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+       
+    }
+}
---
+++ SupplyChain/SupplyChain/ViewModels/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Windows.Input;
+using Dapper;
+
+namespace SupplyChain.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private string _connectionString;
+
+        public ObservableCollection<UserViewModel> Users { get; set; }
+
+        public MainViewModel()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["Main"].ConnectionString;
+
+            Users = new ObservableCollection<UserViewModel>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var users = connection.Query<Entities.User>("SELECT * FROM [User]");
+                foreach (var user in users)
+                {
+                    Users.Add(new UserViewModel(user));
+                }
+            }
+        }
+
+        public ICommand SaveCommand => new CommandHandler(() =>
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var sql = @"UPDATE [User] SET FirstName=@FirstName, LastName=@LastName, UpdatedDate=GETUTCDATE() WHERE Id=@Id";
+
+                foreach (var user in Users)
+                {
+                    connection.Execute(sql, user);
+                }
+            }
+        });
+    }
+}
