d510806e11005b6b10f4abfe03c7124727168941
Yuriy Pelekh <Yuriy.Pelekh@outlook.com>
2019-05-13 14:07:02 +0300

    Update Resources.Designer.cs

--- src/SupplyChain/SupplyChain/Properties/Resources.Designer.cs
+++ src/SupplyChain/SupplyChain/Properties/Resources.Designer.cs
@@ -8,7 +8,8 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SupplyChain.Properties {
+namespace SupplyChain.Properties
+{
     using System;
     
     
