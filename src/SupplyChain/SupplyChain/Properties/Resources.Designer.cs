f069670734ba2e3b8f569473b0da8fbe26bcaa15
Yuriy Pelekh <Yuriy.Pelekh@outlook.com>
2019-05-13 14:13:25 +0300

    Update Resources.Designer.cs

--- src/SupplyChain/SupplyChain/Properties/Resources.Designer.cs
+++ src/SupplyChain/SupplyChain/Properties/Resources.Designer.cs
@@ -53,7 +53,8 @@ namespace SupplyChain.Properties
         ///   обращений к ресурсу с помощью этого класса ресурса со строгой типизацией.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        internal static global::System.Globalization.CultureInfo Culture
+        {
             get
             {
                 return resourceCulture;
