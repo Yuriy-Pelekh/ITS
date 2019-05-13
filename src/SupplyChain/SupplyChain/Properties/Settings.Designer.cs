b2930a17ea5791d80c6e54deb449b84da35e2673
Yuriy Pelekh <Yuriy.Pelekh@outlook.com>
2019-05-13 14:12:09 +0300

    Update Settings.Designer.cs

--- src/SupplyChain/SupplyChain/Properties/Settings.Designer.cs
+++ src/SupplyChain/SupplyChain/Properties/Settings.Designer.cs
@@ -10,10 +10,13 @@
 
 namespace SupplyChain.Properties
 {
+
+
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.8.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
     {
+
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
 
         public static Settings Default
