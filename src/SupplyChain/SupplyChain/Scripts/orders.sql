aad44201a4612d4bbb603738af6e630d262c7d51
Yuriy Pelekh <ypelekh@softserveinc.com>
2019-05-20 01:27:50 +0300

    Fixed update script.

--- src/SupplyChain/SupplyChain/Scripts/orders.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE [Orders](
-    [Id] [int] IDENTITY(1, 1) PRIMARY KEY,
-	CONSTRAINT User_ID FOREIGN KEY (Id)  REFERENCES [User] (Id),
-    [Name] [nvarchar](250) NOT NULL,
-    [CreatedDate] [datetime] NOT NULL)
-GO
