95be8726b2d0543c07689b46de7e599dab3ae193
Yuriy Pelekh <Yuriy.Pelekh@outlook.com>
2019-05-29 17:15:03 +0300

    Revert "Added the buttons "Add" and "Remove""

--- src/SupplyChain/SupplyChain/ViewModels/MainViewModel.cs
+++ src/SupplyChain/SupplyChain/ViewModels/MainViewModel.cs
@@ -3,25 +3,21 @@ using System.Configuration;
 using System.Data.SqlClient;
 using System.Windows.Input;
 using Dapper;
-using System.Windows.Controls;
-using System.Windows;
-using System.Linq;
-using System.ComponentModel;
 
 namespace SupplyChain.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
-
         private string _connectionString;
-        private MainWindow mainWin = Application.Current.Windows.Cast<Window>().FirstOrDefault(window => window is MainWindow) as MainWindow;
+
         public ObservableCollection<UserViewModel> Users { get; set; }
-        public UserViewModel SelectedUser { get; set; }
 
         public MainViewModel()
         {
             _connectionString = ConfigurationManager.ConnectionStrings["Main"].ConnectionString;
+
             Users = new ObservableCollection<UserViewModel>();
+
             using (var connection = new SqlConnection(_connectionString))
             {
                 var users = connection.Query<Entities.User>("SELECT * FROM [User]");
@@ -30,61 +26,19 @@ namespace SupplyChain.ViewModels
                     Users.Add(new UserViewModel(user));
                 }
             }
-
         }
-
 
         public ICommand SaveCommand => new CommandHandler(() =>
         {
-        using (var connection = new SqlConnection(_connectionString))
-            {
-                var sql = @"UPDATE [User] SET FirstName=@FirstName, LastName=@LastName, UpdatedDate=GETUTCDATE() WHERE Id=" + SelectedUser.Id;
-
-                string text1 = mainWin.textBox1.Text;
-                string text2 = mainWin.textBox2.Text;
-                UserViewModel user = new UserViewModel();
-                user.FirstName = text1;
-                user.LastName = text2;
-
-                connection.Execute(sql, user);
-
-            }
-        });
-
-        public ICommand AddCommand => new CommandHandler(() =>
-        {
-
             using (var connection = new SqlConnection(_connectionString))
             {
-                var sql = @"INSERT INTO [User] ([FirstName], [LastName], [UpdatedDate]) VALUES (@FirstName, @LastName,GETUTCDATE())";
+                var sql = @"UPDATE [User] SET FirstName=@FirstName, LastName=@LastName, UpdatedDate=GETUTCDATE() WHERE Id=@Id";
 
-                string text1 = mainWin.textBox1.Text;
-                string text2 = mainWin.textBox2.Text;
-                UserViewModel user = new UserViewModel();
-                user.FirstName = text1;
-                user.LastName = text2;
-
-                connection.Execute(sql, user);
+                foreach (var user in Users)
+                {
+                    connection.Execute(sql, user);
+                }
             }
         });
-
-        public ICommand RemoveCommand => new CommandHandler(() =>
-        {
-
-            using (var connection = new SqlConnection(_connectionString))
-            {
-                var sql = @"DELETE FROM [User] WHERE Id =" + SelectedUser.Id;
-
-                string text1 = mainWin.textBox1.Text;
-                string text2 = mainWin.textBox2.Text;
-                UserViewModel user = new UserViewModel();
-                user.FirstName = text1;
-                user.LastName = text2;
-
-                connection.Execute(sql, user);
-            }
-        });
-
-
     }
 }
